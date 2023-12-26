06890d8138f597f8e03869017caccd577611016a
Rajesh <rajesh.jha25@yahoo.com>
2023-12-26 12:50:49 +0530

    initial commit for cart API

---
+++ Mango.Services.ShoppingCartAPI/MappingConfig.cs
@@ -0,0 +1,15 @@
+﻿using AutoMapper;
+using Mango.Services.ShoppingCartAPI.Models;
+using Mango.Services.ShoppingCartAPI.Models.DTO;
+
+namespace Mango.Services.ShoppingCartAPI
+{
+    public class MappingConfig:Profile
+    {
+        public MappingConfig()
+        {
+            CreateMap<CartHeader,CartHeaderDTO>().ReverseMap();
+            CreateMap<CartDetails, CartDetailsDTO>().ReverseMap();
+        }
+    }
+}
---
+++ Mango.Services.ShoppingCartAPI/Program.cs
@@ -0,0 +1,65 @@
+using Mango.Services.ShoppingCartAPI;
+using Mango.Services.ShoppingCartAPI.Data;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+//automapper
+builder.Services.AddAutoMapper(typeof(MappingConfig));
+//SQL server
+builder.Services.AddDbContext<ApplicationDbContext>(options => {
+
+    options.UseSqlServer(builder.Configuration.GetConnectionString("CartConnection"));
+});
+
+//token validation
+var apiConfig = builder.Configuration.GetSection("ApiSettings:JwtOptions");
+var secret = Encoding.ASCII.GetBytes(apiConfig.GetValue<string>("SecretKey"));
+var issuer = apiConfig.GetValue<string>("Issuer");
+var audience = apiConfig.GetValue<string>("Audience");
+
+builder.Services.AddAuthentication(options =>
+{
+
+    options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+    options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+}).AddJwtBearer(options =>
+{
+    options.TokenValidationParameters = new TokenValidationParameters
+    {
+        ValidateIssuer = true,
+        ValidIssuer = issuer,
+        ValidateAudience = true,
+        ValidAudience = audience,
+        ValidateIssuerSigningKey = true,
+        IssuerSigningKey = new SymmetricSecurityKey(secret)
+    };
+});
+
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+app.UseAuthentication();
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
